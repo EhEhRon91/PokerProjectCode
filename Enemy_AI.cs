05bbba072f2ed56508edc2b66f5d976d22b5f3c6
Aaron Taylor <aaron_t_123@hotmail.com>
2021-08-20 18:31:22 -0400

    Started working on AI.

--- Enemy_AI.cs
+++ Enemy_AI.cs
@@ -21,11 +21,92 @@ public class Enemy_AI : MonoBehaviour
         return hand.hand_evaluated;
     }
 
-    public void FoldOrBet(Hand hand)
+    public bool Fold(Hand hand, int flopCount)
     {
-        if(getHandValue(hand) == Hand.HAND_EVALUATED.HIGH_CARD)
+        if (hand.hand_evaluated >= Hand.HAND_EVALUATED.PAIR)
         {
+            return false;
+        }
+        else
+        {
+            if (hand.card_to_compare.card_value > Card.CARD_VALUE.KING)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
 
+    public bool Stay(Hand hand, int flopCount)
+    {
+        if (hand.hand_evaluated >= Hand.HAND_EVALUATED.PAIR)
+        {
+            return false;
+        }
+        else
+        {
+            if (hand.card_to_compare.card_value > Card.CARD_VALUE.KING)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public int WhatToBet(Player player, Hand hand)
+    {
+        if(hand.hand_evaluated >= Hand.HAND_EVALUATED.FULL_HOUSE)
+        {
+            return player.chips;
+        }
+        else if(hand.hand_evaluated == Hand.HAND_EVALUATED.THREE_OF_A_KIND)
+        {
+            return (int)(player.chips * UnityEngine.Random.Range(0.5f, 0.8f));
+        }
+        else if(hand.hand_evaluated == Hand.HAND_EVALUATED.TWO_PAIR)
+        {
+            if(hand.card_to_compare_two.card_value == Card.CARD_VALUE.ACE)
+            {
+                return (int)(player.chips * UnityEngine.Random.Range(0.3f, 0.5f));
+            }
+            else if(hand.card_to_compare_two.card_value >= Card.CARD_VALUE.TEN)
+            {
+                return (int)(player.chips * UnityEngine.Random.Range(0.2f, 0.4f));
+            }
+            else
+            {
+                return (int)(player.chips * UnityEngine.Random.Range(0.1f, 0.2f));
+            }
+        }
+        else if(hand.hand_evaluated == Hand.HAND_EVALUATED.PAIR)
+        {
+            if (hand.card_to_compare.card_value == Card.CARD_VALUE.ACE)
+            {
+                return (int)(player.chips * UnityEngine.Random.Range(0.05f, 0.1f));
+            }
+            else if (hand.card_to_compare.card_value >= Card.CARD_VALUE.TEN)
+            {
+                return (int)(player.chips * UnityEngine.Random.Range(0.04f, 0.08f));
+            }
+            else
+            {
+                return (int)(player.chips * UnityEngine.Random.Range(0.02f, 0.05f));
+            }
+        }
+        else
+        {
+            if (hand.card_to_compare.card_value == Card.CARD_VALUE.ACE)
+            {
+                return (int)(player.chips * UnityEngine.Random.Range(0.01f, 0.02f));
+            }
+            else if (hand.card_to_compare.card_value >= Card.CARD_VALUE.TEN)
+            {
+                return (int)(player.chips * UnityEngine.Random.Range(0.005f, 0.01f));
+            }
+            else
+            {
+                return (int)(player.chips * UnityEngine.Random.Range(0.0005f, 0.001f));
+            }
         }
     }
 }
